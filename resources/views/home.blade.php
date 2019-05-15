765c015feb4c8332db77e0b5ea50dc9c01ad9a2f
nmbatingal <batingalnarz11@gmail.com>
2019-05-15 18:31:43 +0800

    udpate

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,44 @@
 @extends('layouts.app')
 
+@section('password_change')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8 pb-5">
+            <div class="card">
+                <div class="card-header">Please update your password</div>
+
+                <div class="card-body">
+                    <form method="POST">
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
